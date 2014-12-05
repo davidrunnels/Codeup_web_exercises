1bf82d3e2024af0cc791d553f54af33ee00e8f72
David Runnels <david.runnels@gmail.com>
2014-12-05 14:28:39 -0600

    add email form

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -16,8 +16,24 @@
         <label for="password">Secret Code</label>
         <input id="password" name="password" type="password">
     </p>
+ <p>       
+<button type="submit">Login</button>
+</p>
+<textarea id="email_body" name="email_body" rows="5" cols="40">Content Here</textarea>
+
+<h2>Send E-mail to David:</h2>
+
+<form action="MAILTO:david.runnels@gmail.com" method="post" >
+Name:<br>
+<input type="text" name="name" value="your name"><br>
+E-mail:<br>
+<input type="text" name="mail" value="your email"><br>
+Comment:<br>
+<input type="text" name="comment" value="your comment" size="50"><br><br>
+<input type="submit" value="Send">
+</form>
+    
     <p>
-        <button type="submit">Login</button>
     </p>
 </form>
 </body>
